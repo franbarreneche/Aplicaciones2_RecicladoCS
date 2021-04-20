4877f9fa17fde603a20ae760dd79e52ebbc48eb2
Francisco Barreneche <franbarreneche@gmail.com>
2021-04-20 17:33:26 -0300

    [fix] logo correcto

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
 <div class="box">
+    <h1 class="title">Bienvenido {{auth()->user()->name}}. Tu rol es {{auth()->user()->rol->nombre}}</h1>
     <h2 class="subtitle">Acciones disponibles</h2>
+    @if(auth()->user()->isCoordinador())
+    aca va para que vea el coordinador
+    @else
     <div class="column">
     <a class="button is-large" href="{{route('ciudadanos.index')}}">
         <span class="icon is-medium">
@@ -16,12 +20,6 @@
         </span>
         <span>Centros</span>
     </a>
-      <button class="button is-large">
-        <span class="icon is-medium">
-            <i class="fas fa-trash"></i>
-        </span>
-        <span>Reciclados</span>
-      </button>
-    </div>
+    @endif
 </div>
 @endsection
